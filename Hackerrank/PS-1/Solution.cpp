291cd6553ea0bbbaa025467dd9fd38ef5588c246
Nadib Rana <codecrafersnadib@gmail.com>
2024-11-29 17:55:31 +0600

    Create Solution.cpp

---
+++ Hackerrank/PS-1/Solution.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int A , B , C;
+    cin >> A >> B >> C;
+    int sum = A  + B + C;
+    cout << sum << endl;
+    return 0;
+}
