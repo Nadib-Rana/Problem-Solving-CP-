0298118f40660dc4f30ff08ea39710d4f5667afc
Nadib Rana <codecrafersnadib@gmail.com>
2024-11-30 16:00:02 +0600

    Update solve.cpp

--- vjudge.net/PS-4/solve.cpp
+++ vjudge.net/PS-4/solve.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;  
+    long long num, max = -1e18, min = 1e18;  
+    for (int i = 0; i < n; i++) {
+        cin >> num;  
+        if (num > max) 
+          max = num;
+        if (num < min)
+          min = num;
+    }
+    cout << max << " " << min << endl;
+    return 0;
+}
 
