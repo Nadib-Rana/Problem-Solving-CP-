9596d4f97a3368589efa876c3a4a6652195efaf6
Nadib Rana <codecrafersnadib@gmail.com>
2024-11-30 18:48:03 +0600

    Update solve.cpp

--- vjudge.net/PS-9/solve.cpp
+++ vjudge.net/PS-9/solve.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  int N, F_Pay, paid, paid_back = 0;
+  cin >> N;
+  
+  F_Pay = N * 800; 
+  paid_back = (N / 15) * 200; 
+  
+  paid = F_Pay - paid_back;
+  
+  cout << paid << endl; 
+  return 0;
+}
 
