998c98dbd546a5c294481fedd2d64818dd44c415
Nadib Rana <codecrafersnadib@gmail.com>
2024-11-30 19:39:52 +0600

    Update solve.cpp

--- vjudge.net/PS-11/solve.cpp
+++ vjudge.net/PS-11/solve.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, storage = 5; 
+    cin >> t;
+
+    for (int i = 1; i <= t; i++) {
+        long long N; 
+        cin >> N;
+
+        int need_B = (N + storage - 1) / storage;
+
+        
+        cout << need_B << endl;
+    }
+
+    return 0;
+}
 
