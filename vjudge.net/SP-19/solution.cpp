49bee5c4709cc7e90275781042c139bd92340cc3
Nadib Rana <codecrafersnadib@gmail.com>
2024-12-04 19:54:17 +0600

    Update solution.cpp

--- vjudge.net/SP-19/solution.cpp
+++ vjudge.net/SP-19/solution.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+using namespace std;
+int main (){
+    int T;
+    cin >> T;
+  
+    for (int i = 0; i < T ; i++){
+        int N;
+        cin >> N;
+        
+        int sum = 0;
+        for (int j = N; j > 0; j/=10){
+            sum = sum + (j%10);
+        }
+        cout << sum <<endl;
+    }
+}
