1c87c8c75bb5f2e81cc8be51414a4837309c242f
Nadib Rana <codecrafersnadib@gmail.com>
2024-12-05 13:08:48 +0600

    Update solve.cpp

--- vjudge.net/PS-12/solve.cpp
+++ vjudge.net/PS-12/solve.cpp
@@ -1,1 +1,34 @@
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a; 
+    cin >> b; 
+
+    double y = static_cast<double>(b) / a; 
+    int Cy = ceil(y); 
+
+    cout << Cy << endl; 
+
+    return 0;
+}
+// *****************************************************************************
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b; 
+    int Cy = 0; 
+    while (a <= b) {
+        a = a*3; 
+        b = b*2; 
+        Cy++;   
+    }
+    cout << Cy << endl; 
+    return 0;
+}
